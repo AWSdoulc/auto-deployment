f0957492481bd7e2b459e4b7ef417071ba271475
Tim <69984159+theduolc@users.noreply.github.com>
2025-07-15 22:41:19 +0200

    Update app.py

--- my-python-app/app.py
+++ my-python-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask on Kubernetes!"
+    return "Die CD Pipeline funktioniert!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
