334c9bc750209398d1b15d39f317279c7bb35682
theduolc <jptims88@gmail.com>
2025-07-17 23:41:47 +0200

    Update deploy workflow, app.py, requirements; entferne README.md in neuer-ordner

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-# app.py
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from EC2 at " + __import__("socket").gethostname()
+    return "Hello, World from Flask on k3s!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=5000)
